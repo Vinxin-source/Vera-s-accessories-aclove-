eebdc510fe0f49fc0148e26cff362de9d9dc8728
Vinxin-source <johnvinxin@gmail.com>
2026-09-11 12:30:08 +0100

    Add files via upload

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,1 +1,20 @@
+import { SiteHeader } from "@/components/SiteHeader";
+import { SiteFooter } from "@/components/SiteFooter";
+import { ProductDetail } from "@/components/ProductDetail";
 
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <SiteHeader />
+      <main className="mx-auto w-full max-w-6xl flex-1 px-4 py-10">
+        <ProductDetail slug={slug} />
+      </main>
+      <SiteFooter />
+    </div>
+  );
+}
